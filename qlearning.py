fix: espilon formula

qlearning.py
@@ -95,7 +95,7 @@ class QLearningAgent:
         action = self.legal_actions[0]
 
         # BEGIN SOLUTION
-        if np.random.random() < self.epsilon:
+        if np.random.random() <= self.epsilon:
             action = random.choice(self.legal_actions)
         else:
             action = self.get_best_action(state)

qlearning_eps_scheduling.py
@@ -43,11 +43,15 @@ class QLearningAgentEpsScheduling(QLearningAgent):
         action = self.legal_actions[0]
 
         # BEGIN SOLUTION
-        epsilion_c = (self.epsilon_start - self.epsilon_end) / self.epsilon_decay_steps * self.timestep
-        if np.random.random() < self.epsilon_start - epsilion_c:
+        if self.timestep == 0:
+            self.reset()
+        
+        if np.random.random() <= self.epsilon:
             action = random.choice(self.legal_actions)
         else:
             action = self.get_best_action(state)
+    
+        self.epsilon -= (self.epsilon_start - self.epsilon_end) / self.epsilon_decay_steps
         self.timestep = (self.timestep + 1) % self.epsilon_decay_steps
         # END SOLUTION
 

taxi.py
@@ -87,7 +87,7 @@ assert np.mean(rewards[-100:]) > 0.0
 
 
 agent = QLearningAgentEpsScheduling(
-    learning_rate=0.5, epsilon=0.25, gamma=0.99, legal_actions=list(range(n_actions))
+    learning_rate=0.1, epsilon=0.1, gamma=0.6, legal_actions=list(range(n_actions))
 )
 
 rewards = []
@@ -97,7 +97,6 @@ for i in range(1000):
         print("mean reward", np.mean(rewards[-100:]))
 
 assert np.mean(rewards[-100:]) > 0.0
-
 # TODO: créer des vidéos de l'agent en action
 
 